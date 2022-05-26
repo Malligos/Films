7454ad8a2b851e2f5a6114e5b1cc501c6c306e80
Malligos <butssergei7778@gmail.com>
2022-05-26 22:54:55 +0300

    for in

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-let name = 'Ilya';
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
 
-alert(`Hello ${1}`);
-
-alert(`Hello ${'name'}`);
-
-alert(`Hello ${name}`);
+for (let key in options){
+    if(typeof(options[key]) === 'object'){
+        for(let i in options[key]){
+        console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+    }else{
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
